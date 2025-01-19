61c228dfdbf4a86b819817141c07de98d44f75d3
Dipankar <drupd17@gmail.com>
2025-01-19 20:57:44 +0530

    update

--- p8.c
+++ p8.c
@@ -1,33 +1,88 @@
-#include <stdio.h>
+// #include <stdio.h>
+// #include <mpi.h>
+
+// int main(int argc, char **argv)
+// {
+//     MPI_Init(&argc, &argv);
+
+//     int rank, size;
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+//     int dims[2] = {2, 2}, coords[2], north, south, east, west;
+//     int periods[2] = {0, 0};
+//     MPI_Comm cart_comm;
+
+//     // Ensure that the number of processes is divisible by the number of dimensions
+//     if (size != 4)
+//     {
+//         MPI_Finalize();
+//         return -1;
+//     }
+
+//     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
+//     MPI_Cart_coords(cart_comm, rank, 2, coords);
+
+//     MPI_Cart_shift(cart_comm, 0, 1, &north, &south);
+//     MPI_Cart_shift(cart_comm, 1, 1, &west, &east);
+
+//     printf("P%d has coords: %d %d and neighbours: %d %d %d %d\n", rank, coords[0], coords[1], north, south, east, west);
+
+//     MPI_Finalize();
+//     return 0;
+// }
+
 #include <mpi.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
-
-    int rank, size;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int dims[2] = {2, 2}, coords[2], north, south, east, west;
-    int periods[2] = {0, 0};
+    int rank, numtasks;
     MPI_Comm cart_comm;
 
-    // Ensure that the number of processes is divisible by the number of dimensions
-    if (size != 4)
-    {
-        MPI_Finalize();
-        return -1;
-    }
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Dynamically calculate grid dimensions based on numtasks (assuming a square grid)
+    int dims[2] = {0, 0}; // Allow MPI to decide the best dimensions
+    MPI_Dims_create(numtasks, 2, dims);
+
+    // Ensure grid is non-periodic
+    int periods[2] = {0, 0};
+    int coords[2], nbrs[4]; // Neighbors: UP, DOWN, LEFT, RIGHT
+
+    // Create a Cartesian communicator
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
     MPI_Cart_coords(cart_comm, rank, 2, coords);
 
-    MPI_Cart_shift(cart_comm, 0, 1, &north, &south);
-    MPI_Cart_shift(cart_comm, 1, 1, &west, &east);
+    // Get neighbors in all directions
+    MPI_Cart_shift(cart_comm, 0, 1, &nbrs[0], &nbrs[1]); // UP, DOWN
+    MPI_Cart_shift(cart_comm, 1, 1, &nbrs[2], &nbrs[3]); // LEFT, RIGHT
 
-    printf("P%d has coords: %d %d and neighbours: %d %d %d %d\n", rank, coords[0], coords[1], north, south, east, west);
+    printf("Rank %d at coords (%d, %d) has neighbors: UP=%d, DOWN=%d, LEFT=%d, RIGHT=%d\n",
+           rank, coords[0], coords[1], nbrs[0], nbrs[1], nbrs[2], nbrs[3]);
 
+    int outbuf = 15, inbuf[4] = {-1, -1, -1, -1}; // Send 15 to all neighbors
+    MPI_Request reqs[8];
+
+    // Initiate non-blocking sends and receives
+    for (int i = 0; i < 4; i++)
+    {
+        if (nbrs[i] != MPI_PROC_NULL)
+        {
+            MPI_Isend(&outbuf, 1, MPI_INT, nbrs[i], 0, cart_comm, &reqs[i]);
+            MPI_Irecv(&inbuf[i], 1, MPI_INT, nbrs[i], 0, cart_comm, &reqs[i + 4]);
+        }
+        else
+        {
+            reqs[i] = MPI_REQUEST_NULL;
+            reqs[i + 4] = MPI_REQUEST_NULL;
+        }
+    }
+    MPI_Waitall(8, reqs, MPI_STATUSES_IGNORE);
+    printf("Rank %d received data from neighbors: UP=%d, DOWN=%d, LEFT=%d, RIGHT=%d\n",
+           rank, inbuf[0], inbuf[1], inbuf[2], inbuf[3]);
     MPI_Finalize();
     return 0;
 }
--- p9.c
+++ p9.c
@@ -1,81 +1,203 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <mpi.h>
+
+// #define TAG 100
+
+// int main(int argc, char **argv)
+// {
+
+//     int rank, size;
+//     int sendData = 10;
+//     int recvData;
+
+//     MPI_Request req[2];
+//     MPI_Status stat[2];
+
+//     MPI_Init(&argc, &argv);
+
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+//     if (size != 2)
+//     {
+//         MPI_Finalize();
+//         return -1;
+//     }
+
+//     if (rank == 0)
+//     {
+//         MPI_Send(
+//             &sendData,
+//             1,
+//             MPI_INT,
+//             1,
+//             TAG,
+//             MPI_COMM_WORLD);
+
+//         printf("P1 Sent data %d\n ", sendData);
+
+//         MPI_Irecv(
+//             &recvData,
+//             1,
+//             MPI_INT,
+//             1,
+//             TAG,
+//             MPI_COMM_WORLD,
+//             &req[0]);
+//         printf("P1 start non blocking recv \n");
+
+//         MPI_Wait(&req[0], &stat[0]);
+//         printf("P1 receive data \n");
+//     }
+//     else if (rank == 1)
+//     {
+
+//         MPI_Isend(
+//             &sendData,
+//             1,
+//             MPI_INT,
+//             1,
+//             TAG,
+//             MPI_COMM_WORLD,
+//             &req[0]);
+//         printf("P2 start non blocking send\n");
+
+//         MPI_Recv(
+//             &recvData,
+//             1,
+//             MPI_INT,
+//             0,
+//             TAG,
+//             MPI_COMM_WORLD,
+//             &stat[1]);
+//         printf("P2 receive %d \n", recvData);
+
+//         MPI_Wait(&req[0], &stat[0]);
+//         printf("P2 finish sending data\n");
+//     }
+
+//     MPI_Finalize();
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 
-#define TAG 100
+void blocking_communication(int rank, int numtasks);
+void nonblocking_communication(int rank, int numtasks);
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-
-    int rank, size;
-    int sendData = 10;
-    int recvData;
-
-    MPI_Request req[2];
-    MPI_Status stat[2];
+    int numtasks, rank;
 
     MPI_Init(&argc, &argv);
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (size != 2)
+    // Perform blocking communication
+    blocking_communication(rank, numtasks);
+
+    // Synchronize before non-blocking communication
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Perform non-blocking communication
+    nonblocking_communication(rank, numtasks);
+
+    MPI_Finalize();
+    return 0;
+}
+
+void blocking_communication(int rank, int numtasks)
+{
+    int rc, inmsg, next, prev;
+    double st_blocking, et_blocking;
+    MPI_Status Stat;
+
+    st_blocking = MPI_Wtime(); // Start time for blocking communication
+
+    // Determine next and previous ranks for circular communication
+    if (rank == 0)
+        prev = numtasks - 1;
+    else
+        prev = rank - 1;
+
+    if (rank == numtasks - 1)
+        next = 0;
+    else
+        next = rank + 1;
+
+    if (rank % 2 == 0)
     {
-        MPI_Finalize();
-        return -1;
+        // Even ranks send and receive
+        printf("Rank %d (Blocking): Sending message '%d' to Rank %d\n", rank, rank, next);
+        rc = MPI_Send(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+
+        rc = MPI_Recv(&inmsg, 1, MPI_INT, prev, 1, MPI_COMM_WORLD, &Stat);
+        printf("Rank %d (Blocking): Received message '%d' from Rank %d\n", rank, inmsg, prev);
     }
+    else
+    {
+        // Odd ranks receive and send
+        rc = MPI_Recv(&inmsg, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &Stat);
+        printf("Rank %d (Blocking): Received message '%d' from Rank %d\n", rank, inmsg, prev);
+
+        printf("Rank %d (Blocking): Sending message '%d' to Rank %d\n", rank, rank, next);
+        rc = MPI_Send(&rank, 1, MPI_INT, next, 1, MPI_COMM_WORLD);
+    }
+
+    et_blocking = MPI_Wtime(); // End time for blocking communication
+
+    // Synchronize all processes before printing the time
+    MPI_Barrier(MPI_COMM_WORLD);
 
     if (rank == 0)
     {
-        MPI_Send(
-            &sendData,
-            1,
-            MPI_INT,
-            1,
-            TAG,
-            MPI_COMM_WORLD);
-
-        printf("P1 Sent data %d\n ", sendData);
-
-        MPI_Irecv(
-            &recvData,
-            1,
-            MPI_INT,
-            1,
-            TAG,
-            MPI_COMM_WORLD,
-            &req[0]);
-        printf("P1 start non blocking recv \n");
-
-        MPI_Wait(&req[0], &stat[0]);
-        printf("P1 receive data \n");
+        // Print the time for blocking communication
+        printf("\nTime taken by Blocking send/receive: %lf seconds\n", et_blocking - st_blocking);
     }
-    else if (rank == 1)
+}
+
+void nonblocking_communication(int rank, int numtasks)
+{
+    int inmsg, next, prev;
+    double st_nonblocking, et_nonblocking;
+    MPI_Request reqs[2];
+
+    st_nonblocking = MPI_Wtime(); // Start time for non-blocking communication
+
+    // Initialize next and previous ranks for circular communication
+    if (rank == 0)
+        prev = numtasks - 1;
+    else
+        prev = rank - 1;
+
+    if (rank == numtasks - 1)
+        next = 0;
+    else
+        next = rank + 1;
+
+    // Non-blocking operations
+    printf("Rank %d (Non-blocking): Non-blocking receive from Rank %d\n", rank, prev);
+    MPI_Irecv(&inmsg, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &reqs[0]);
+
+    printf("Rank %d (Non-blocking): Non-blocking send to Rank %d\n", rank, next);
+    MPI_Isend(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD, &reqs[1]);
+
+    // Wait for the non-blocking operations to complete
+    MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
+    MPI_Wait(&reqs[1], MPI_STATUS_IGNORE);
+
+    // printf("Rank %d (Non-blocking): Received message '%d' from Rank %d\n", rank, inmsg, prev);
+
+    et_nonblocking = MPI_Wtime(); // End time for non-blocking communication
+
+    // Synchronize all processes before printing the time
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank == 0)
     {
-
-        MPI_Isend(
-            &sendData,
-            1,
-            MPI_INT,
-            1,
-            TAG,
-            MPI_COMM_WORLD,
-            &req[0]);
-        printf("P2 start non blocking send\n");
-
-        MPI_Recv(
-            &recvData,
-            1,
-            MPI_INT,
-            0,
-            TAG,
-            MPI_COMM_WORLD,
-            &stat[1]);
-        printf("P2 receive %d \n", recvData);
-
-        MPI_Wait(&req[0], &stat[0]);
-        printf("P2 finish sending data\n");
+        // Print the time for non-blocking communication
+        printf("Time taken by Non-blocking send/receive: %lf seconds\n", et_nonblocking - st_nonblocking);
     }
-
-    MPI_Finalize();
 }
